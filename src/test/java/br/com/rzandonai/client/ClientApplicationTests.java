c27296515c6e9884e08a993070fab8357586be39
Ricardo Zandonai <squall@outlook.com>
2021-06-17 19:41:08 -0300

    Adiciona Redis e jedis
    Adiciona o Redis e o jedis e ajusta o teste para nâo falhar ao subir a aplicação com o redis

--- src/test/java/br/com/rzandonai/client/ClientApplicationTests.java
+++ src/test/java/br/com/rzandonai/client/ClientApplicationTests.java
@@ -1,13 +1,48 @@
 package br.com.rzandonai.client;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ImportAutoConfiguration(classes = { CacheAutoConfiguration.class, RedisAutoConfiguration.class })
 class ClientApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+
+
+
+	@TestConfiguration
+	static class EmbeddedRedisConfiguration {
+
+		private final RedisServer redisServer;
+
+		public EmbeddedRedisConfiguration(@Value("${spring.redis.port}") int redisPort) {
+			this.redisServer = new RedisServer(redisPort);
+		}
+
+		@PostConstruct
+		public void startRedis() {
+			redisServer.start();
+		}
+
+		@PreDestroy
+		public void stopRedis() {
+			this.redisServer.stop();
+		}
+	}
 }
